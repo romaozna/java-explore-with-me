de0e722c3a81d4871e62aa9d384b1690edf6ea05
akhmerov.rr <akhmerov.rr@yandex.ru>
2023-05-06 08:35:59 +0500

    Этап 3. fix: codestyle

--- main/src/main/java/ru/practicum/ewm/comment/repository/CommentRepository.java
+++ main/src/main/java/ru/practicum/ewm/comment/repository/CommentRepository.java
@@ -9,5 +9,6 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> getAllByAuthorIdAndEventId(Long authorId, Long eventId, Pageable pageable);
+
     List<Comment> getAllByAuthorId(Long authorId, Pageable pageable);
 }
--- main/src/main/java/ru/practicum/ewm/comment/service/CommentService.java
+++ main/src/main/java/ru/practicum/ewm/comment/service/CommentService.java
@@ -7,10 +7,16 @@ import java.util.List;
 
 public interface CommentService {
     CommentDto create(Long userId, Long eventId, CommentDto commentDto);
+
     CommentDto update(Long commentId, Long userId, Long eventId, CommentDto commentDto);
+
     CommentDto getById(Long userid, Long commentId);
+
     List<CommentDto> getAllByEventId(Long userId, Long eventId, Pageable pageable);
+
     List<CommentDto> getAllByUserId(Long userId, Pageable pageable);
+
     void deleteById(Long commentId);
+
     void deleteByUser(Long userId, Long commentId);
 }
